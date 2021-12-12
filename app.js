c317a6b807d3cd1129f6372cae22a778d3c3ed7c
Pobx <geidtiphong.s@allrightbox.com>
2021-12-12 13:28:14 +0700

    add function reply

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
-app.post ('/webhook', (req, res) => res.sendStatus(200))
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+  const replyToken = req.body.events[0].replyToken;
+  const userId = req.body.events[0].source.userId;
+  const originalMessage = req.body.events[0].message.text;
+  const events = req.body.events;
+
+  reply({ replyToken, userId, originalMessage, events });
+  res.sendStatus(200);
+});
+
 app.listen(port);
+
+function reply({ replyToken, userId, originalMessage, events }) {
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer 8b6402529e6cec77aef143ad7e96cfca",
+  };
+
+  const body = JSON.stringify({
+    replyToken,
+    messages: [
+      { type: "text", text: `userId(${userId}) say: ${originalMessage}` },
+      { type: "text", text: JSON.stringify(events) },
+    ],
+  });
+
+  request.post(
+    {
+      url: "https://api.line.me/v2/bot/message/reply",
+      headers,
+      body,
+    },
+    (err, res, body) => console.log(`Status = ${res.statusCode}`)
+  );
+}
